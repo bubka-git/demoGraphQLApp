a9484b28d8c19d590e7c26a226b888a4546a1228
Kaushik Biswas <kaushik.biswas@outlook.com>
2020-09-29 15:59:24 +0530

    Initial Draft

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "demoGraphQLApp/graph/model"
+
+// This file will not be regenerated automatically.
+//
+
+//Resolver injects the model.Todo dependency.
+type Resolver struct {
+	todos []*model.Todo
+}
